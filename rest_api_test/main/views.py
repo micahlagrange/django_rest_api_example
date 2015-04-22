617b0d60fa02dea542ed426f1abe70252bcfa61a
multivoxmuse <turner.micah@gmail.com>
2015-04-21 22:38:58 -0600

    updated dosctring

--- rest_api_test/main/views.py
+++ rest_api_test/main/views.py
@@ -1,5 +1,6 @@
 """
     This is a very brief and readable REST API written in python and backed by django.
+    Plain, vanilla django. no modules or extra packages needed. NOT DJANGO REST FRAMEWORK!
      You can run it as is by using manage.py runserver, and doing a GET to "localhost:portnumber"
      for example, GET to localhost:8080/users/123/ to get a dictionary like:
         {"name" "bob", "mail": "mackieb@email.com", "blurb": "Wow, Bob Mackie!"}
